5c4755cc03bd24c476d874ad50239d0e04cc7e2d
aridai <jakubkaczma5@gmail.com>
2021-10-31 20:47:13 +0100

    Maybe it's the resize code being wrongfully applied to the banner?

--- js/resize-iframe.js
+++ js/resize-iframe.js
@@ -1,11 +1,13 @@
 function autoResize(ele) {
     var newheight;
     var newwidth;
+    newheight = ele.contentWindow.document.body.scrollHeight;
+    newwidth = ele.contentWindow.document.body.scrollWidth;
 
-    if (document.getElementById) {
-        newheight = ele.contentWindow.document.body.scrollHeight;
-        newwidth = ele.contentWindow.document.body.scrollWidth;
+    if (document.getElementsByClassName("banner")) {
+        ele.height = (newheight) + "px";
+    } else {
+        ele.height = "100%";
     }
-    ele.height = "100%";
     ele.width = (newwidth) + "px";
 }
